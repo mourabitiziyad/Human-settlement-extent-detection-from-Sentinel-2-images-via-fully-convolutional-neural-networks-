4904f23387542c34135a257af1b3b794d0d3b81b
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2024-06-06 15:17:04 +0200

    chore: Update img2map.py with import and code formatting

--- img2map.py
+++ img2map.py
@@ -2,6 +2,7 @@
 # @Email:  chunping.qiu@tum.de
 # @Last modified time: 2020-01-27T15:18:56+01:00
 
+import tensorflow as tf
 import sys
 from img2mapC import img2mapC
 
@@ -15,53 +16,53 @@ from scipy import stats
 
 import modelSelection
 from keras.optimizers import Nadam
-nadam = Nadam(lr=0.0002, beta_1=0.9, beta_2=0.999, epsilon=None, schedule_decay=0.004)
+nadam = Nadam(lr=0.0002, beta_1=0.9, beta_2=0.999,
+              epsilon=None, schedule_decay=0.004)
 
-import tensorflow as tf
-import os
 
-os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-config = tf.ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.9
-session = tf.Session(config=config)
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+# config = tf.ConfigProto()
+# config.gpu_options.per_process_gpu_memory_fraction = 0.9
+# session = tf.Session(config=config)
 
 ########################################################
-modelPath='./data/'
-fileD='./data/img/'
+modelPath = './data/'
+fileD = './data/img/'
 
 nnList = [12]
 ########################################################
-MapfileD=modelPath+'pre/'
+MapfileD = modelPath+'pre/'
 if not os.path.exists(MapfileD):
-	os.makedirs(MapfileD)
+    os.makedirs(MapfileD)
 ########################################################
 patch_shape = (128, 128, 10)
-step=patch_shape[0]
+step = patch_shape[0]
 params = {'dim_x': patch_shape[0],
-		   'dim_y': patch_shape[1],
-		   'dim_z': patch_shape[2],
-		   'step': step,
-		   'Bands': [1,2,3,4,5,6,7,8,11,12],
-		   'scale':10000.0,
-		   'nanValu':999}
+          'dim_y': patch_shape[1],
+          'dim_z': patch_shape[2],
+          'step': step,
+          'Bands': [0, 1, 2, 3, 4, 5, 6, 7, 8, 11],
+          'scale': 10000.0,
+          'nanValu': 999}
 
-img2mapCLass=img2mapC(**params);
+img2mapCLass = img2mapC(**params)
 
-for nn in nnList:#
+for nn in nnList:
 
-	model, dataFlow = modelSelection.modelSelection(nn, nadam, patch_shape=patch_shape)
-	modelName =  modelPath + '_'+ str(nn)+"_8weights.best.hdf5"
+    model, dataFlow = modelSelection.modelSelection(
+        nn, nadam, patch_shape=patch_shape)
+    modelName = modelPath + '_' + str(nn)+"_8weights.best.hdf5"
 
-	print(modelName)
-	model.load_weights(modelName, by_name=False)
+    print(modelName)
+    model.load_weights(modelName, by_name=False)
 
-	files=glob2.glob(fileD+'*.tif')
+    files = glob2.glob(fileD + '*.tif*')
 
-	for file in files:
+    for file in files:
 
-			filename=os.path.basename(file);
-			mapFile = MapfileD+filename[:-4]+'_'+ str(nn)
+        filename = os.path.basename(file)
+        mapFile = MapfileD+filename[:-4]+'_' + str(nn)
 
-			print(params['Bands'])
-			print(files, mapFile)
-			img2mapCLass.img2Bdetection_ovlp([file], model, mapFile, nn=nn)
+        print(params['Bands'])
+        print(files, mapFile)
+        img2mapCLass.img2Bdetection_ovlp([file], model, mapFile, nn=nn)
